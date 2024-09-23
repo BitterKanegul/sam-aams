e16f6c3760eee8adc82102efce3cdfe86bc53464
BitterKanegul <15769643+BitterKanegul@users.noreply.github.com>
2024-09-23 18:05:15 -0400

    Create cesk-simpl.rkt

---
+++ cesk-simpl.rkt
@@ -0,0 +1,57 @@
+#lang racket
+#|
+
+State machine
+<x, r, s ,k>             -> <v, r', s, k> where s(r(x)) = (v, r')
+<(e0e1), r, s, k>           -> <e0, r, s, ar(e1, r, k)>
+<v, r, s, ar(e, p',k)>      -> <e, r', s, fn(v, r, k)>
+<v, r, s, fn((lx.e), p', k)> -> <e, r'[x -> a], s[a -> (v,r)], k> where a !∈ dom(s)
+
+|#
+
+;; Extending and looking up environment
+(define (lookup-state x r s)
+  (define res (hash-ref s (hash-ref r x)))
+  (values (car res) (cdr res))
+ )
+(define (extend-env x v r r+ s)
+  (define new-addr (gensym))
+  (define r-upd (hash-set r x new-addr))
+  (define s-upd (hash-set s new-addr (values v r)))
+  (values r-upd s-upd)
+ )
+
+(define (step state)
+  (match state
+    ;<x, r, s ,k>             -> <v, r', s, k> where s(r(x)) = (v, r')
+    [`((ref ,(? symbol? x)) ,r ,s ,k)
+      (define-values (v r+) (lookup-state x r s))
+      `(,v ,r+ ,s ,k)
+     ]
+    ;<(e0e1), r, s, k>           -> <e0, r, s, ar(e1, r, k)>
+    [`((app ,e0 ,e1) ,r ,s ,k)
+      `(,e0 ,r ,s (ar ,e1 ,r ,k))
+     ]
+    ;<v, r, s, ar(e, r',k)>      -> <e, r', s, fn(v, r, k)>
+    [`(,v ,r ,s (ar ,e ,r+ ,k))
+     `(,e ,r+ ,s (fn ,v ,r ,k))
+     ]
+    ;<v, r, s, fn((lx.e), r', k)> -> <e, r'[x -> a], s[a -> (v,r)], k> where a !∈ dom(s)
+    [`(,v ,r ,s fnc((λ,x ,e) ,r+ ,k))
+     (define-values (r-upd s-upd) (extend-env x v r r+ s))
+     `(,e ,r-upd ,s-upd ,k)
+     ]
+    ))
+;; Start from (expr, {}, {}, mt)
+(define (run-prog expr)
+  (define r-init (hash))
+  (define s-init (hash))
+  (let loop ([i 1] [state `(,expr ,r-init ,s-init mt)])
+   (let ([state1 (step state)])
+   (displayln state1)
+   (let ([inp (read-line)])
+     (when (not (equal? inp "q"))
+       (loop (add1 i) state1))))))
+
+
+  
